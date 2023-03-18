3d036f6a8bfb09e5c7bcaccd1f270b5f04c0086d
HackerTheBestYTB <56880221+HackerTheBestYTB@users.noreply.github.com>
2023-03-19 01:03:42 +0700

    Update Loader.lua

--- cloud/Loader.lua
+++ cloud/Loader.lua
@@ -1,9 +1,3 @@
-local doors = 6839171747
-local evade = 9872472334
-local spacesailors = 5000143962
-local michaelszombies = 8054462345
-local gutp = 2746687316
-
 local tableofplaceid = {
 	"2746687316",--gutp
 	"5000143962",--spacesailors
@@ -16,29 +10,29 @@ local tableofplaceid = {
 
 --function success()
 	if table.find(tableofplaceid, tostring(game.PlaceId)) then
-		if table.find(tostring(game.PlaceId), "2746687316") then --gutp
+		if tostring(game.PlaceId) == "2746687316" then --gutp
 			loadstring(game:HttpGet(("https://raw.githubusercontent.com/HackerTheBestYTB/Script/test/GUTP.lua")))()
 		end
-		if table.find(tostring(game.PlaceId), "5000143962") then --spacesailors
+		if tostring(game.PlaceId) == "5000143962" then --spacesailors
 			loadstring(game:HttpGet(("")))()
 		end
-		if table.find(tostring(game.PlaceId), "9872472334") then --evade
+		if tostring(game.PlaceId) == "9872472334" then --evade
 			loadstring(game:HttpGet(("")))()
 		end
-		if table.find(tostring(game.PlaceId), "8054462345") then --michaelszombies
+		if tostring(game.PlaceId) == "8054462345" then --michaelszombies
 			loadstring(game:HttpGet(("")))()
 		end
-		if table.find(tostring(game.PlaceId), "6516141723") or table.find(tostring(game.PlaceId), "6839171747") then --doors
-			loadstring(game:HttpGet(("https://raw.githubusercontent.com/HackerTheBestYTB/Script/test/DoorsNew.lua")))()
+		if tostring(game.PlaceId) == "6516141723" or tostring(game.PlaceId) == "6839171747" then --doors
+		loadstring(game:HttpGet(("https://raw.githubusercontent.com/HackerTheBestYTB/Script/test/DoorsNew.lua")))()
 		end
 	end
 --end
 
 
---[=[
+
 if _G.Key1002 ~= nil and _G.Key2000 ~= nil and _G.Key2001 ~= nil then
 	success()
 else
 	fail()
 end
-]=]
+
